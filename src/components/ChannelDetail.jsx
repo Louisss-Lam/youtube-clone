152d7d4263d78eb6bd2cbf3febe65aaebd1b294e
Louisss-Lam <louislam617@gmail.com>
2022-10-26 15:00:28 +0100

    fix: videoCard function

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParmas } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './'; 
 
 const ChannelDetail = () => {
   return (
